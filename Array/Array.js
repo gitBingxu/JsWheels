384948412c1c72fade91fbe121d8a11451870ea7
bingxu <984386667@qq.com>
2020-03-19 17:07:21 +0800

    [fix] 更适合业务代码

--- Array/Array.js
+++ Array/Array.js
@@ -1,10 +1,18 @@
+/*
+ * @Author: Bing Xu
+ * @Date: 2019-12-12 15:38:58
+ * @LastEditTime: 2020-03-19 17:06:46
+ * @LastEditors: Bing Xu
+ * @Description: 
+ * @FilePath: /JsWheels/Array/Array.js
+ */
 class TreeNode {
   static counter = 0
-  constructor(label, pid){
+  constructor(pid, label, id = `${TreeNode.counter++}`, children = []){
     this.pid = pid
-    this.id = TreeNode.counter++
+    this.id = id
     this.label = label
-    this.children = []
+    this.children = children
   }
 }
 
@@ -13,7 +21,7 @@ function getIndex (node, label) {
     return item.label === label
   })
   if (index < 0) {
-    node.children.push(new TreeNode(label, node.id))
+    node.children.push(new TreeNode(node.id, label))
     return getIndex(node, label)
   }
   return index
@@ -28,16 +36,16 @@ function getNode (tree, condition, obj) {
 }
 
 /**
- * @param {Array} source // 源数组
+ * @param {Array<Object>} source // 源数组
  * @param {string} label // 最后叶子节点 展示的label
  * @param {string} condition // 每一级分类用的 tag
  * @return {TreeNode}
  */
 function arrayTotree (source, label, ...condition) {
-  const root = new TreeNode('root', -1)
+  const root = new TreeNode(-1, 'root')
   source.forEach(item => {
     const node = getNode(root, condition, item)
-    node.children.push(new TreeNode(item[label], node.id))
+    node.children.push(new TreeNode(node.id, item[label], item.id))
   })
   return root
 }
